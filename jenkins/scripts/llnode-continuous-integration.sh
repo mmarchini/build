980928d7ff8262190501ee205936e6d90d6e1e91
Gibson Fahnestock <gibfahn@gmail.com>
2017-12-02 00:28:34 +0000

    squash! misc fixes

--- jenkins/scripts/llnode-continuous-integration.sh
+++ jenkins/scripts/llnode-continuous-integration.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -ex
 
-"$(dirname $0)"/common/getNode.sh # Load colors.
+rm -rf llnode
 
-export UNZIP_DIR=`ls |grep "node" |grep -v "gz"`
-cd $WORKSPACE/node-*/bin
+"$(dirname $0)"/common/getNode.sh # Download node tarball.
 
-export PATH=$PWD:$PATH
-cd $WORKSPACE
+export PATH="$(cd $WORKSPACE/node-*/bin; pwd):$PATH"
+
 export NPM_CONFIG_USERCONFIG=$WORKSPACE/npmrc
 export NPM_CONFIG_CACHE=$WORKSPACE/npm-cache
 node -v
@@ -14,21 +13,20 @@ npm -v
 export npm_loglevel=error
 npm set progress=false
 ls
-git clone https://github.com/$GIT_REPO.git llnode
+
+git clone --depth=1 -b $GIT_BRANCH https://github.com/$GIT_REPO.git llnode
 cd llnode
-if [ $GIT_BRANCH != master ]; then
-	git fetch origin $GIT_BRANCH:testBranch
-	git checkout testBranch
-fi
 
 [ $(uname) = Darwin ] && git clone --depth=1 -b release_39 https://github.com/llvm-mirror/lldb.git lldb
 # Initialize GYP
 git clone https://chromium.googlesource.com/external/gyp.git tools/gyp
+
 # Configure
 [ $(uname) = Linux ] && GYP_ARGS="-Dlldb_dir=/usr/lib/llvm-3.8/" || GYP_ARGS=""
 ./gyp_llnode $GYP_ARGS
+
 # Build
-make -C out/ -j9
+make -C out/ -j${JOBS:-1}
 
 npm install
 npm test
